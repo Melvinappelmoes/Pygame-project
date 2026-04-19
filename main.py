57dabf2622a2ee978ab6ea89fb69580637e29ff4
Melvin Bel <Mbel@leerling.stadenesch.nl>
2026-04-19 21:44:20 +0200

    Gemaakt dat de tetriminoes afgebeeld kunnen worden en alles opgedeeld in losse bestanden

--- main.py
+++ main.py
@@ -1,54 +1,23 @@
 import pygame
+from tetriminoes import *
+from classes import *
+from variables import *
 pygame.init()
 
-WHITE = (255, 255, 255)
-BLACK = (0, 0, 0)
-YELLOW = (255, 255, 0)
-LIGHT_BLUE = (0,191,255)
-PURPLE = (128,0,128)
-ORANGE = (255,165,0)
-BLUE = (0,0,255)
-GREEN = (0, 255, 0)
-RED = (255, 0, 0)
+def make_grid():
+    # maakt de grid door, door het hele bord in rijen te gaan met stapsgrootte = grid_size
+    for x in range(0, window_width // grid_size):
+        for y in range(0, window_height // grid_size):
+            rect = pygame.Rect(x*grid_size, y*grid_size, grid_size, grid_size)
+            pygame.draw.rect(screen, GREY, rect, 1)
 
-size = 300, 600
-grid_size = 30
-
-screen = pygame.display.set_mode(size)
-clock = pygame.time.Clock()
-
-class Tetrimino():
-    def __init__(self, shape):
-        self.x = 150
-        self.y = -60
-        self.shape = shape
-        self.rotation = 0
-
-o_t = [[1, 1],
-       [1, 1]],
-
-i_t_1 = [[[1],
-       [1],
-       [1],
-       [1]],
-
-       [[1, 1, 1, 1]]]
-
-i_t_2 = [[[0, 0, 1, 0],
-         [0, 0, 1, 0],
-         [0, 0, 1, 0],
-         [0, 0, 1, 0]],
-         
-         [[0, 0, 0, 0],
-          [0, 0, 0, 0],
-          [1, 1, 1, 1],
-          [0, 0, 0, 0]]]
-
+# main gameloop (moet miss in een class)
 running = True
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-    screen.fill(BLACK)
-    clock.tick(60)
+    i_t.draw()
+    make_grid()
+    clock.tick(1)
     pygame.display.flip()
